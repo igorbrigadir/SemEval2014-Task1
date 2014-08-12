58969d3430af0c220a95969ed743f60c6f8728ad
igorbrigadir <igor.brigadir@gmail.com>
2014-08-12 10:51:28 +0000

    w2v Tests

--- code/cdsm/src/main/java/org/insight/semeval/ModelComparator.java
+++ code/cdsm/src/main/java/org/insight/semeval/ModelComparator.java
@@ -3,10 +3,13 @@ package org.insight.semeval;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.LineIterator;
+import org.insight.word2vec.DiskWord2Vec;
 import org.insight.word2vec.Word2Vec;
 import org.insight.word2vec.util.GloveTextModelLoader;
 import org.insight.word2vec.util.ModelLoader;
@@ -46,37 +49,61 @@ public class ModelComparator {
 		
 	//	models.add(GloveTextModelLoader.load("/home/igor/git/SemEval2014-Task1/models/vectors.6B.200d"));
 	//	models.add(ModelLoader.load("/home/igor/git/word2vec-java/src/main/resources/vectors.bin"));
-		models.add(GloveTextModelLoader.load("/home/igor/git/SemEval2014-Task1/models/vectors.840B.300d"));
+	//	models.add(GloveTextModelLoader.load("/home/igor/git/SemEval2014-Task1/models/vectors.840B.300d"));
 		
+	//	models.add();
 		
-		for (Word2Vec model : models) {
+		DiskWord2Vec model = new DiskWord2Vec("/fastdata/wikipedia-pubmed-and-PMC-w2v.bin");
+		
+		// Preload vectors:
+		
+		HashSet<String> words = new HashSet<String>();
+		for (Sentence sentence : sentences) {
+			words.addAll(Arrays.asList(sentence.sA.toLowerCase().split(" ")));
+			words.addAll(Arrays.asList(sentence.sB.toLowerCase().split(" ")));
+		}
+		
+		System.out.println("Total Words to Scan: " + words.size());
+		
+		model.preCache(words);
+
+						
+	//	for (Word2Vec model : models) {
 			
-			File outputFile = new File("/home/igor/git/SemEval2014-Task1/results/Insight/Insight_w2v_glove840b.txt");
+			File outputFile = new File("/home/igor/git/SemEval2014-Task1/results/Insight/Insight_w2v_wiki.txt");
 
 			List<Sentence> results_sentences = new ArrayList<Sentence>();
 
+			// Write Results Header:
+			
+			FileUtils.writeStringToFile(outputFile, "pair_ID\tentailment_judgment\trelatedness_score" + "\n", false);
+
+			
 			for (Sentence sentence : sentences) {
 				float similarity = interpolateScore(
 						VectorMath.cosineSimilarity(
 								VectorMath.normalize(model.sentenceVector(sentence.sA)),
 								VectorMath.normalize(model.sentenceVector(sentence.sB)))
 						);
-				results_sentences.add(sentence.withSimilarity(similarity));
-			}
+				
+				Sentence newSentence = sentence.withSimilarity(similarity);
+				
+				//results_sentences.add();
 			
-			
-			// Write Results Header:
-			
-			FileUtils.writeStringToFile(outputFile, "pair_ID\tentailment_judgment\trelatedness_score" + "\n", false);
-						
-			for (Sentence result : results_sentences) {
-				String resultline = String.format("%s\tNA\t%s\n", result.id, result.similarity);
+				String resultline = String.format("%s\tNA\t%s\n", newSentence.id, newSentence.similarity);
 				FileUtils.writeStringToFile(outputFile, resultline, true);
+			
+			
 			}
+			
+			
+							
+			//for (Sentence result : results_sentences) {
+			//}
 
 		}
 
-	}
+	//}
 
 	/*
 	 * Cosine Similarity to 1..5 Scale:
